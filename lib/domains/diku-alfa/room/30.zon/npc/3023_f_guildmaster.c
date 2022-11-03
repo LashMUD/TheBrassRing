8b13ce23014da679a073b46e6b4d21edc13844b3
Christopher Coker <ccoker@intergate.com>
2022-11-03 12:13:59 -0400

    updates

--- lib/domains/diku-alfa/room/30.zon/npc/3023_f_guildmaster.c
+++ lib/domains/diku-alfa/room/30.zon/npc/3023_f_guildmaster.c
@@ -82,13 +82,22 @@ void init() {
 }
 
 int AdvanceDude(mixed arg){
-    int level,xp,qp;
+    int level,dlev,xp,qp,totpoints;
     int desired_level,required_xp,required_qp;
     mixed *statlist;
     mapping this_stat;
     int statclass, statlevel, i;
+    string str;
+    string *ptmp = ({});
+    string *stmp = ({});
+    string *mtmp = ({});
+    int *plevels = ({});
+    int *slevels = ({});
+    int *mlevels = ({});
+    int *psorted, *ssorted, *msorted;
 
     statlist = this_player()->GetStats();
+
 
     if(this_player()->GetKeyName() == "guest"){
         this_object()->eventForce("say I don't promote temporary players.");
@@ -99,7 +108,11 @@ int AdvanceDude(mixed arg){
         this_object()->eventForce("say You are confusing me.");
         return 1;
     }
-
+    if(adminp(this_player()) || creatorp(this_player())){
+        this_object()->eventForce("say Do you really NEED to be promoted?");
+        this_object()->eventForce("shake");
+        return 1;
+    }
     if(level > 19){
         this_object()->eventForce("say Whoa there, big "
                 "stuff. Advancement past level 20 is the "
@@ -108,51 +121,108 @@ int AdvanceDude(mixed arg){
         return 1;
     }
 
-    xp = this_player()->GetExperiencePoints();
-    if(!qp = this_player()->GetQuestPoints()) qp = 0;
-    desired_level = level+1;
-    required_xp = advancement[desired_level]["xp"];
-    if(!required_qp = advancement[desired_level]["qp"]) required_qp = 0;
-    if(!REQUIRE_QUESTING) required_qp = 0;
-    this_object()->eventForce("say Level "+desired_level+" "
-            "with the title of \""+advancement[desired_level]["title"]+"\" "
-            "requires "+required_xp+" experience points and "+
-            required_qp+" quest points.");
+    if(XP_ADVANCE){
+        xp = this_player()->GetExperiencePoints();
+        if(!qp = this_player()->GetQuestPoints()) qp = 0;
+        desired_level = level+1;
+        required_xp = advancement[desired_level]["xp"];
+        if(!required_qp = advancement[desired_level]["qp"]) required_qp = 0;
+        if(!REQUIRE_QUESTING) required_qp = 0;
+        this_object()->eventForce("say Level "+desired_level+" "
+                "with the title of \""+advancement[desired_level]["title"]+"\" "
+                "requires "+required_xp+" experience points and "+
+                required_qp+" quest points.");
 
-    if( xp > required_xp-1 && qp > required_qp-1){
-        this_object()->eventForce("say Congratulations! "
-                "You are promoted to level "+desired_level+" and "
-                "have earned the name "+this_player()->GetName()+" "
-                +advancement[desired_level]["title"]+".");
+        if( xp > required_xp-1 && qp > required_qp-1){
+            this_object()->eventForce("say Congratulations! "
+                    "You are promoted to level "+desired_level+" and "
+                    "have earned the name "+this_player()->GetName()+" "
+                    +advancement[desired_level]["title"]+".");
 
-        this_player()->ChangeLevel(desired_level);
-        this_player()->AddTrainingPoints(desired_level);
-        this_player()->AddTitle(advancement[desired_level]["title"]);
-        this_player()->RemoveTitle(advancement[desired_level-1]["title"]);
+            this_player()->ChangeLevel(desired_level);
+            this_player()->AddTrainingPoints(desired_level);
+            this_player()->AddTitle(advancement[desired_level]["title"]);
+            this_player()->RemoveTitle(advancement[desired_level-1]["title"]);
 
-        this_player()->save_player(this_player()->GetKeyName());
+            this_player()->save_player(this_player()->GetKeyName());
 
-        if(level == MAX_NEWBIE_LEVEL){
-            write("\nCorbyn raises his hand and sternly points to you.\n");
-            say("\nCorbyn raises his hand and sternly points to "+
-                    this_player()->GetName()+".\n");
-            this_object()->eventForce("say "+this_player()->GetName()+","+
-                    " you are no longer a newbie. From now on, you will need"+
-                    " a light source to see in the dark. From now on, you will"+
-                    " not understand languages you haven't learned. You have"+
-                    " earned this promotion, and now face the future as a"+
-                    " real adventurer.");
+            if(level == MAX_NEWBIE_LEVEL){
+                write("\nDirk raises his hand and sternly points to you.\n");
+                say("\nDirk raises his hand and sternly points to "+
+                        this_player()->GetName()+".\n");
+                this_object()->eventForce("say "+this_player()->GetName()+","+
+                        " you are no longer a newbie. From now on, you will need"+
+                        " a light source to see in the dark. From now on, you will"+
+                        " not understand languages you haven't learned. You have"+
+                        " earned this promotion, and now face the future as a"+
+                       " real adventurer.");
+            }
+    
+            return 1;
         }
 
-        return 1;
+        else this_object()->eventForce("say I'm sorry, "+
+                this_player()->GetName()+", but you have not "
+                "fulfilled all the requirements of level "+
+                desired_level+". Please come back and try "
+                "again once you have fulfilled them.");
+        return 0;
     }
 
-    else this_object()->eventForce("say I'm sorry, "+
-            this_player()->GetName()+", but you have not "
-            "fulfilled all the requirements of level "+
-            desired_level+". Please come back and try "
-            "again once you have fulfilled them.");
-    return 0;
+    if(SKILL_ADVANCE){
+        level = this_player()->GetLevel() * 15;
+        dlev = (this_player()->GetLevel())+1;
+        //tell_player("lash", "level is "+level+" and dlev is "+dlev); 
+        
+        if(!this_player() || !playerp(this_player()) || !this_player()->GetSkills()){
+            //tell_player("lash", "No levels!");
+            return 0;
+        }
+        /* get and sort primary skills and levels */
+        ptmp = this_player()->GetPrimarySkills();
+        foreach(str in ptmp){
+            plevels += ({this_player()->GetBaseSkillLevel(str)});
+        }
+        plevels = sort_array(plevels,-1);
+        /* get and sort secondary skills and levels */
+        stmp = this_player()->GetSecondarySkills();
+        foreach(str in stmp){
+            slevels += ({this_player()->GetBaseSkillLevel(str)});
+        }    
+        slevels = sort_array(slevels,-1);
+        /* get and sort minor skills and levels */
+        mtmp = this_player()->GetMinorSkills();
+        foreach(str in mtmp){
+            mlevels += ({this_player()->GetBaseSkillLevel(str)});
+        }
+        mlevels = sort_array(mlevels,-1);
+        totpoints = plevels[0] + plevels[1] + plevels[2] + plevels[3] + plevels[4] +
+                    slevels[0] + slevels[1] + slevels[2] +
+                    mlevels[0] + mlevels[1] + mlevels[2] + mlevels [3] + mlevels [4];
+        //tell_player("lash","totpoints is "+totpoints);
+        }                
+        if(totpoints >= level){
+            this_object()->eventForce("say Congratulations! "
+            "You are promoted to level "+dlev+" and "
+            "have earned the name "+this_player()->GetName()+" "
+            +advancement[dlev]["title"]+".");
+
+            this_player()->ChangeLevel(dlev);
+            this_player()->AddTrainingPoints(dlev);
+            this_player()->AddTitle(advancement[dlev]["title"]);
+            this_player()->RemoveTitle(advancement[dlev-1]["title"]);
+
+            this_player()->save_player(this_player()->GetKeyName());
+        return 1;
+        }
+        else
+            this_object()->eventForce("tell "+this_player()->GetName()+" I'm sorry, "+
+            this_player()->GetName()+", but you need to "
+            "earn at least "+(level-totpoints)+" more skill points "
+            "before you can advance to level "+dlev+". "
+            "Please come back and try again once you get "
+            "more skill points.");
+        return 0;
 }
 
 string GetLevelTitle(int level){
