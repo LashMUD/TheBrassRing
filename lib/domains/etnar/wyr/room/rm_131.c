934d861b1476e878471b063d0c779984b1d97f60
Christopher Coker <ccoker@intergate.com>
2022-11-03 11:38:43 -0400

    updates

--- lib/domains/etnar/wyr/room/rm_131.c
+++ lib/domains/etnar/wyr/room/rm_131.c
@@ -8,19 +8,22 @@
 
 inherit LIB_ROOM;
 
+int ReadSign();
+
 static void create() {
 
     room::create();
     SetClimate("indoors");
     SetAmbientLight(30);
     SetShort("%^BOLD%^The Common Room%^RESET%^");
-    SetLong("   You enter a large dimly lit room. There are rectangular and round\n"+
-        "tables scattered throughout. In the southern part of the room a large\n"+
-        "fireplace holds a crackling fire spewing thick black smoke up into the\n"+
-        "chimney. The room structure does not look very stable and four large pillars\n"+
-        "have been erected to hold the ceiling in place. The smell of beer and smoke\n"+
-        "permeate the air. To the north is a back room and there is a pair of double\n"+
-        "doors to the west. To the east lies the entrance to the inn.");
+    SetLong("   You enter a large, dimly lit room. There are rectangular and round "
+            "tables scattered throughout. In the southern part of the room, a large "
+            "fireplace holds a crackling fire, spewing thick black smoke up into the "
+            "chimney. The room structure does not look very stable, and four large "
+            "pillars have been erected to hold the ceiling in place. The smell of "
+            "beer and smoke permeate the air. To the north is a back room, and there "
+            "is a pair of double doors to the west. To the east lies the entrance to "
+            "the inn.");
     SetItems( ([ 
                 "north" : "A pair of double doors. A sign is posted here.",
                 "east" : "The entrance to the Cyclops Inn.",
@@ -37,6 +40,9 @@ static void create() {
                 ({"pillars", "pillar"}) : 
                     "The pillars are made out of a stone that you realize is not common to Wyr.",
                ] ));
+    SetRead( ([
+            "sign" : (:ReadSign:),
+            ]) );
     SetExits( ([
                 "north" : "/domains/etnar/wyr/room/rm_132",
                 "east" : "/domains/etnar/wyr/room/rm_130",
@@ -44,8 +50,12 @@ static void create() {
                 //"northwest" : "/domains/etnar/wyr/room/rm_172",
                 "southwest" : "/domains/etnar/wyr/room/rm_174",
                ] ));
-
 }
+
+mixed ReadSign() {
+    return (mixed)this_player()->eventPage("/domains/etnar/wyr/text/rentsign.txt");
+}
+
 void init(){
    ::init();
 }
