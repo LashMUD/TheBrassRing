b99cc21a5ed5167cfcb60e649ea34c4c947e8efe
Christopher Coker <ccoker@intergate.com>
2022-11-23 10:30:03 -0500

    the acolyte in Henlis' shrine

--- lib/domains/etnar/wyr/npc/winifred_barton.c
+++ lib/domains/etnar/wyr/npc/winifred_barton.c
@@ -18,9 +18,9 @@ int WieldClub(){
     }
     else{
         new("/domains/etnar/wyr/weap/wooden_club")->eventMove(this_object());
-        tell_room(environment(),"%^BOLD%^%^RED%^The Acolyte pulls out a club "
-            "from under his robes and wields it with terrible fury!%^RESET%^");
-        this_object()->eventForce("say By The hand of Balcor I shall slay you!");
+        tell_room(environment(),"%^BOLD%^%^RED%^Winifred pulls out a club "
+            "from under her robes and wields it with terrible fury!%^RESET%^");
+        this_object()->eventForce("say By The hand of Balcor, I shall slay you!");
         this_object()->eventForce("wield club");
     }
     return 1;
@@ -29,26 +29,31 @@ int WieldClub(){
 static void create() {
     sentient::create();
 
-    SetKeyName("the Acolyte");
-    SetId(({"acolyte"}));
-    //SetAdjectives(({"non-player", "non player"}));
-    SetShort("An Acolyte");
-    SetLong("A pious acolyte.");
+    SetKeyName("the acolyte");
+    SetId(({"winifred", "Winifred", "barton", "Barton", "acolyte"}));
+    SetAdjectives(({"non-player", "non player"}));
+    SetShort("Winifred Barton");
+    SetLong("A pious young acolyte of Balcor, the wargod, stands before you. "
+        "Winifred is earnest in her studies of the teachings of battlemastery. "
+        "She wears light magenta robes depicting the image of Balcor holding "
+        "a lightning bolt in his hand.");
     SetCanBite(0);
     SetRace("human");
     SetClass("cleric");
     SetLevel(5);
-    SetMelee(1);
-    SetGender("male");
-    SetMorality(2500);
+    SetGender("female");
+    SetMorality(1500);
     AddCurrency("gold", 5);
     SetLanguage("common", 100);
     SetDefaultLanguage("common");
     SetInventory( ([
         "/domains/etnar/wyr/weap/wooden_club" : "1",
-       ] ));
+        ] ));
+    SetAction(50, ({ (:CheckWielded:), }) );
     SetCombatAction(100, ({ (: WieldClub :),
-       }) );
+        }) );
+    SetFactions( ([ "The Strike of Balcor" : ({5, 5}),
+        ]) );
 }
 
 int CheckWielded(){
