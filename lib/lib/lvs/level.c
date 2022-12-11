40de81b76526bf0b88671c65096ec74070ad2666
Christopher Coker <ccoker@intergate.com>
2022-12-11 09:24:25 -0500

    refined skill based adjustments for level changes

--- lib/lib/lvs/level.c
+++ lib/lib/lvs/level.c
@@ -6,7 +6,8 @@
  *    The Brass Ring mud
  *     - added this header
  *     - added functions for skill advancement system
- *    last modified: 20/05/20
+ *     - refined skill based adjustments for level changes
+ *    last modified: 22/12/11 year/month/day
  */ 
 
 
@@ -38,7 +39,7 @@ int collect_moduli(int mod, int *range){
 
 varargs int ChangeLevel(int i){
     mapping skills, stats, moduli = ([]);
-    int current_level, desired_level;
+    int current_level, desired_level, delta_level, cs;
     object subject = this_object();
 
     current_level = subject->GetLevel();
@@ -57,32 +58,56 @@ varargs int ChangeLevel(int i){
     skills = subject->GetSkillsMap();
     stats = subject->GetStatsMap();
 
-    if(XP_ADVANCE){
-        foreach(mixed key, mixed val in skills){
-            if(skills[key]["class"] > 3) skills[key]["class"] = 4;
-            subject->SetSkill(key, 
-                    skills[key]["level"] + moduli[skills[key]["class"]],
-                    skills[key]["class"]);
+    
+    foreach(mixed key, mixed val in skills){
+        if(skills[key]["class"] > 3) skills[key]["class"] = 4;
+        subject->SetSkill(key, 
+                skills[key]["level"] + moduli[skills[key]["class"]],
+                skills[key]["class"]);
         }
-
+    
+    if(XP_ADVANCE){
         foreach(mixed key, mixed val in stats){
             if(stats[key]["class"] > 3) stats[key]["class"] = 4;
             subject->SetStat(key,
                     stats[key]["level"] + moduli[stats[key]["class"]],
                     stats[key]["class"]);
         }
-
         subject->SetLevel(desired_level);
 
         if(interactive() && find_object(INSTANCES_D)){
             INSTANCES_D->SendWhoUpdate(this_object()->GetKeyName());
         }
     }
-    /* added by lash (ccoker) */
-    if(SKILL_ADVANCE){
-        subject->AddCustomizationPoints();
+    /* add customization points for level increase*/
+    if(SKILL_ADVANCE && (desired_level > current_level)) {
+        delta_level = desired_level - current_level;
+        for(i = 0; i < delta_level; i++) {
+           subject->AddCustomizationPoints();
+        }
+        
         subject->SetLevel(desired_level);
-
+        if(interactive() && find_object(INSTANCES_D)){
+            INSTANCES_D->SendWhoUpdate(this_object()->GetKeyName());
+        }
+    }
+    /*decreasing stats for level loss*/
+    if(SKILL_ADVANCE && (desired_level <= current_level)) {
+        tell_player("lash", "desired_level is "+desired_level+" and current_level is "+current_level);
+        foreach(mixed key, mixed val in stats){
+            if(stats[key]["class"] > 3) stats[key]["class"] = 4;
+            subject->SetStat(key,
+                    stats[key]["level"] + moduli[stats[key]["class"]],
+                    stats[key]["class"]);
+        }
+        /* if they player foolishly retained custom stat points
+         * over levels they lose half as a penalty
+         */  
+        if(subject->GetCustomStats() >= 2) {
+            cs = subject->GetCustomStats()/2;
+            subject->AddCustomizationPoints(-cs);
+        }            
+        subject->SetLevel(desired_level);
         if(interactive() && find_object(INSTANCES_D)){
             INSTANCES_D->SendWhoUpdate(this_object()->GetKeyName());
         }
