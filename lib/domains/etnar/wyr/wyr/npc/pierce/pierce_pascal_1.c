12488409fca6bccffbc1fb800f00561e558778e7
Christopher Coker <ccoker@intergate.com>
2023-01-07 06:43:00 -0500

    fixed a bug where pascal would get "stuck" after combat

--- lib/domains/etnar/wyr/wyr/npc/pierce/pierce_pascal_1.c
+++ lib/domains/etnar/wyr/wyr/npc/pierce/pierce_pascal_1.c
@@ -1,5 +1,6 @@
-/* /domains/etnar/wyr/wyr/npc/gate_guard_ab.c
- * Albert Derby - a gate guard in the village of Wyr
+/* /domains/etnar/wyr/wyr/npc/pierce_pascal.c
+ * alternative pierce pascal captain of the wyr 
+ * militia in the village of Wyr
  * based on The Dead Souls Mud Library
  * maintained by Cratylus http://www.dead-souls.net
  * for use in The Brass Ring Mud
